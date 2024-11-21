70e31214333163f3a1539152c5c3e6962ae3e43d
Bhoomika1345 <bhoomika.j1990@gmail.com>
2024-11-21 14:00:45 +0530

    fixed issue

--- components/CustomizedDataTable.tsx
+++ components/CustomizedDataTable.tsx
@@ -33,6 +33,7 @@ export default function CustomizedDataTable() {
   const gridRef = useRef(null);
   const [aggrementsData, setAggrementsData] = useState([]);
 
+  //
   useEffect(() => {
     const countIds = {};
     let temp = agreements.map((e) => {
